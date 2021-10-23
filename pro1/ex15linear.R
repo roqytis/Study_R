ac9a9afe2dac188614acf974bf373a0a022384d7
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-23 19:29:55 +0900

    cars dataset

--- pro1/ex15linear.R
+++ pro1/ex15linear.R
@@ -1,4 +1,5 @@
 #선형회귀 : cars dataset
+
 head(cars,3) #speed(속도) dist(제동거리)
 
 cor(cars$speed, cars$dist) #0.8068949
@@ -10,7 +11,7 @@ summary(model)
 coef(model) #회귀 계수수
 fitted(model) #예측값 : 각 speed 값에 dist에측값
 
-residuals(model)[1:4] #잔차차
+residuals(model)[1:4] #잔차
 
 # cat('실제값 :', fitted(model)[1:4])
 # cat('예측값 : ', cars$dist[1:4])
@@ -24,3 +25,4 @@ summary(model)$r.squared #결정계수
 
 #예측값
 predict(model, newdata = data.frame(speed=10))
+
